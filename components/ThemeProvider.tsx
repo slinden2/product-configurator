ce3573db8104416b90038581a13426ca96431c2b
slinden2 <39922426+slinden2@users.noreply.github.com>
2025-02-12 13:30:27 +0100

    Migration completed

--- components/ThemeProvider.tsx
+++ components/ThemeProvider.tsx
@@ -1,8 +1,14 @@
 "use client";
 
 import * as React from "react";
-import { ThemeProvider as NextThemesProvider } from "next-themes";
-import { type ThemeProviderProps } from "next-themes/dist/types";
+import dynamic from "next/dynamic";
+
+const NextThemesProvider = dynamic(
+  () => import("next-themes").then((mod) => mod.ThemeProvider),
+  { ssr: false }
+);
+
+type ThemeProviderProps = React.ComponentProps<typeof NextThemesProvider>;
 
 export function ThemeProvider({ children, ...props }: ThemeProviderProps) {
   return <NextThemesProvider {...props}>{children}</NextThemesProvider>;
